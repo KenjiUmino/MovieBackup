98456278b71b72111736352c73a8b5946522ba61
KenjiUmino <85372551+KenjiUmino@users.noreply.github.com>
2023-01-06 17:21:18 +0100

    cleaned up directory paths

--- iso2mkv.sh
+++ iso2mkv.sh
@@ -8,8 +8,8 @@ y="${x#'./'}"
 echo "Starting conversion of $y -> ${y%'.ISO'}"
 # make dir of /output/path/ISO_FILE/*.mkv
 # but only if dir does not exist yet
-if [[ ! -d "/run/media/umino/Backup6T/DVD_MOVIES/${y%'.ISO'}" ]]; then
-mkdir -p "/run/media/umino/Backup6T/DVD_MOVIES/${y%'.ISO'}"
+if [[ ! -d "/path/to/DVD_ISO_DIR/${y%'.ISO'}" ]]; then
+mkdir -p "/path/to/DVD_ISO_DIR/${y%'.ISO'}"
 # convert them all, ignore tracks shorter than minlength (if you only care about the main movie)
 makemkvcon --minlength=1200 mkv iso:"$x" all "/run/media/umino/Backup6T/DVD_MOVIES/${y%'.ISO'}"; 
 # report if finished
